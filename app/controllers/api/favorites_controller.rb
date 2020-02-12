0f3c61f0057fbe10c601d0a5294decf7be70460f
Purnama Sidi Rahayu <purnamasrahayu@gmail.com>
2020-02-12 22:59:50 +0700

    adding Dockerfile

--- app/controllers/api/favorites_controller.rb
+++ app/controllers/api/favorites_controller.rb
@@ -1,6 +1,6 @@
 class Api::FavoritesController < ApplicationController
     before_action :authenticate_user
-    before_action :set_movie, only: [:create, :destroy]
+    before_action :set_movie, only: [:create, :destroy, :show]
 
     def set_movie
         @movie = Movie.find(params[:id])
@@ -9,6 +9,12 @@ class Api::FavoritesController < ApplicationController
     def create
         current_user.favorite!(@movie)
         render json: @movie
+    end
+
+    def show
+        @movies = current_user.movies
+        value = @movies.include? @movie
+        render :json => value.to_json
     end
     
     def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     resources :movies
     post 'favorites/:id' => 'favorites#create'
     delete 'favorites/:id' => 'favorites#destroy'
+    get 'favorites/:id' => 'favorites#show'
     get 'favorites' => 'favorites#index'
     get 'movie_by_genre/:genre' => 'movies#show_by_genre'
     get 'title/:title' => 'movies#find_by_title'
